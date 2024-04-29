37f1574fe4348c8e34987c7555070d8326467bd3
chigan <chigan@Mac-mini-chigan.local>
2024-04-29 23:10:21 +0500

    add comment and method 'getWord'

--- trans.js
+++ trans.js
@@ -1,17 +1,24 @@
 class TranslationManager {
     constructor(words, defaultLang='ru', delimiters = ['{%', '%}'], useLocalStorage = true) {
+        // Constructor to initialize TranslationManager object
+        // words: Object containing translations for different languages
+        // defaultLang: Default language if current language is not set
+        // delimiters: Delimiters used for dynamic text replacement
+        // useLocalStorage: Flag to determine if localStorage should be used for language storage
         this.words = words;
         this.defaultLang = defaultLang;
         this.delimiters = delimiters;
-        this.currentLang = this.getCurrentLanguage() || defaultLang;
+        this.currentLang = this.getCurrentLanguage() || defaultLang; // Set current language
         this.useLocalStorage = useLocalStorage;
     }
 
     getCurrentLanguage() {
+        // Get the current language from localStorage if available, otherwise use defaultLang
         return this.useLocalStorage ? localStorage.getItem('language') || this.defaultLang : this.defaultLang;
     }
 
     setCurrentLanguage(lang) {
+        // Set the current language and store it in localStorage if enabled
         if (this.useLocalStorage) {
             localStorage.setItem('language', lang);
         }
@@ -19,16 +26,26 @@ class TranslationManager {
     }
 
     initTransWord() {
+        // Initialize translation of static text
         this.translateStaticText();
     }
 
+    getWord(key) {
+        // Get translation for a specific key in the current language
+        // If translation not found, return the key itself
+        const langWords = this.words[this.currentLang];
+        return langWords && langWords[key] ? langWords[key] : key;
+    }
+
     findWordIndices(text) {
+        // Find the indices of the dynamic text within delimiters
         const pattern = new RegExp(`${this.delimiters[0]}\\s*(.*?)\\s*${this.delimiters[1]}`);
         const match = text.match(pattern);
         return match ? { key: match[1].trim(), startIndex: match.index, endIndex: match.index + match[0].length - 1 } : null;
     }
 
     translateStaticText() {
+        // Translate static text elements on the page
         const elementsWithTrans = document.querySelectorAll('[trans], [trans-place], [trans-btn], [data-trans-dynamic], [delimiters-trans]');
         const langWord = this.words[this.currentLang] || {};
 
@@ -38,6 +55,7 @@ class TranslationManager {
 
             switch (attributeName) {
                 case 'trans':
+                    // Translate text attribute
                     const textToInsert = langWord[attributeValue] || '';
                     const insertToStart = element.hasAttribute('trans-to-start');
                     if (insertToStart) {
@@ -48,20 +66,25 @@ class TranslationManager {
                     break;
                 
                 case 'trans-place':
+                    // Translate placeholder attribute
                     element.placeholder = langWord[attributeValue] || '';
                     break;
                 
                 case 'trans-btn':
+                    // Translate button text content
                     const buttonText = langWord[attributeValue] || '';
                     element.textContent = buttonText;
                     break;
                 
                 case 'data-trans-dynamic':
+                    // Translate dynamic text content
                     element.textContent = langWord[attributeValue] || '';
                     break;
 
                 case 'delimiters-trans':
-                    this.translateTextWithDelimiters(element, langWord); // Вызываем метод для замены текста между разделителями
+                    // Translate text within delimiters
+                    this.translateTextWithDelimiters(element, langWord);
+                    break;
                 
                 default:
                     break;
@@ -70,19 +93,21 @@ class TranslationManager {
     }
 
     translateTextWithDelimiters(element, words) {
+        // Translate text within delimiters
         let textContent = element.textContent.trim()
-        const findResutl = this.findWordIndices(textContent);
+        const findResult = this.findWordIndices(textContent);
 
-        if (!findResutl)
+        if (!findResult)
             return null;
 
-        const {key, startIndex, endIndex} = findResutl;
+        const { key, startIndex, endIndex } = findResult;
         const newText = textContent.slice(0, startIndex) + (words[key] || key) + textContent.slice(endIndex+1);
 
         element.textContent = newText;
     }
 
     getAttributeName(element) {
+        // Get the name of the translation attribute for an HTML element
         switch (true) {
             case element.hasAttribute('trans'):
                 return 'trans';
